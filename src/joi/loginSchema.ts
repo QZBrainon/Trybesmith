130c4c11ed59944e92438de23a036027d3a3877e
Brainon Queiroz <brainonqueiroz@gmail.com>
2022-10-14 00:36:37 -0300

    Req07 - Added classe and level to validator

--- src/joi/loginSchema.ts
+++ src/joi/loginSchema.ts
@@ -3,6 +3,8 @@ import Joi from 'joi';
 const loginSchema = Joi.object({
   username: Joi.string().required(),
   password: Joi.string().required(),
+  level: Joi.number().required(),
+  classe: Joi.string().required(),
 });
 
 export default loginSchema;
